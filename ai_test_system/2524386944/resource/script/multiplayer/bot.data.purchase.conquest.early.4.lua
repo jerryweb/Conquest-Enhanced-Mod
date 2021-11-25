6faf666616a2826ba74f968d60e02d9e3a534a6b
Jerry Webb <ljwebbdev8800@gmail.com>
2021-11-25 00:12:22 -0800

    Got object systemm to work

---
+++ ai_test_system/2524386944/resource/script/multiplayer/bot.data.purchase.conquest.early.4.lua
@@ -0,0 +1,167 @@
+Purchases["campaign_capture_the_flag"] = {
+	{--purchase 1 starting recon wave c 1 по 5 бой
+		divisionName = "Condor historic division",
+		waveNumber = 1, -- какая по порядку будет
+		Repeat = 6, -- сколько раз должно вызваться, чтобы могло перейти к следующей волне
+		minRepeat = 2,
+		maxRepeat = 4,
+		skipPossible = false,  -- ничего не делает
+		waveDuration = 20, -- длительность того, как долго бот будет вызывать данные войска. Каждый юнит вызывается каждые 3 секунды. Если нужно чтобы было только определенное количество то Repeat * 3 < Wave duration  При этом Repeat говорит сколько раз нужно вызвать юнит.
+		Units = {
+			["ger"] = {--Germany
+			--Wheel_vehicles
+				{priority = 0.1, class = UnitClass.Vehicle, unit = "p204_f"},
+				{priority = 1.0, class = UnitClass.Vehicle, unit = "sdkfz231"},
+				{priority = 2.0, class = UnitClass.Vehicle, unit = "sdkfz221"},
+			--Infantry Squads
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_recon_con(ger)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_recon_motor_con"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_recon_armor_con"},				
+			},
+			["rus"] = {--Russia
+			--Infantry Singles
+			    {priority = 2.5, class = UnitClass.Infantry, unit = "squad_recon_con(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "single_sniper(rus)"},
+			--Wheel_vehicles
+				{priority = 1.0, class = UnitClass.Vehicle, unit = "ba6"},
+			}
+		}
+	},
+	{--purchase 2 mortar
+		waveNumber = 2,
+	    -- Repeat = 2,
+	    minRepeat = 1,
+		maxRepeat = 1,
+	    skipPossible = false,
+	    waveDuration = 2,
+		Units = {
+			["ger"] = {--Germany
+		    --Howitzers
+                {priority = 1.0, class = UnitClass.ATTank, unit = "80mm_sgrw34"},
+			},
+			["rus"] = {--Russia
+			--Howitzers
+                {priority = 1.0, class = UnitClass.ATTank, unit = "82mm_bm37"},
+			}
+		}
+	},
+	{--purchase 3 light tank group
+		waveNumber = 3,
+	    -- Repeat = 10,
+	    minRepeat = 3,
+		maxRepeat = 6,
+	    skipPossible = false,
+	    waveDuration = 20,
+		Units = {
+			["ger"] = {--Germany
+			--Tanks_light
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer1b"},
+				{priority = 2.0, class = UnitClass.Tank, unit = "panzer2c"},
+				{priority = 2.0, class = UnitClass.Tank, unit = "panzer2f"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer35t"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "pz38t_a"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "pz38t_f"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "pz38t_g"},
+			},
+			["rus"] = {--Russia			
+            --Tanks_light
+				{priority = 2.0, class = UnitClass.Tank, unit = "t26_33"},
+				{priority = 2.0, class = UnitClass.Tank, unit = "t26_39"},
+				{priority = 0.5, class = UnitClass.Tank, unit = "t26_31"},				
+			}
+		}
+	},
+	{--purchase 4 medium tank group
+		waveNumber = 4,
+	    -- Repeat = 6,
+	    minRepeat = 2,
+		maxRepeat = 4,
+	    skipPossible = false,
+	    waveDuration = 20,
+		Units = {
+			["ger"] = {--Germany
+			--Tanks_medium
+				{priority = 2.0, class = UnitClass.Tank, unit = "panzer3e"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer3f"},
+				{priority = 0.3, class = UnitClass.Tank, unit = "panzer3j"},
+				{priority = 0.2, class = UnitClass.Tank, unit = "panzer3j1"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer4d"},
+			},
+			["rus"] = {--Russia		
+			--Tanks_medium
+				{priority = 0.8, class = UnitClass.Tank, unit = "t28"},
+				{priority = 0.1, class = UnitClass.Tank, unit = "t3476_40"},
+				{priority = 0.1, class = UnitClass.Tank, unit = "t3476_41"},
+			--Tanks_heavy
+			}
+		}
+	},
+	{--purchase 5 infantry support
+		waveNumber = 5,
+		-- Repeat = 15,
+		minRepeat = 4,
+		maxRepeat = 8,
+        skipPossible = false,		
+		waveDuration = 35,
+		Units = {
+			["ger"] = {--Germany
+			--Infantry Squads
+				{priority = 3.5, class = UnitClass.Infantry, unit = "squad_regular_con(ger)"},
+			},
+			["rus"] = {--Russia
+			--Infantry Squads
+				{priority = 2.5, class = UnitClass.Infantry, unit = "squad_rifleunit_alt_con(rus)"},
+				{priority = 3.5, class = UnitClass.Infantry, unit = "squad_rifleunit_con(rus)"},
+			}
+		}
+	},
+	{--purchase 6 SPG support
+		waveNumber = 6,
+		-- Repeat = 3,
+		minRepeat = 1,
+		maxRepeat = 3,
+        skipPossible = false,		
+		waveDuration = 15,
+		Units = {
+			["ger"] = {--Germany
+			--SPG
+				{priority = 0.5, class = UnitClass.ATTank, unit = "panzerjager1"},
+				{priority = 0.1, class = UnitClass.ArtilleryTank, unit = "bison"},
+			},
+			["rus"] = {--Russia
+			--SPG
+				{priority = 0.1, class = UnitClass.ATTank, unit = "zis30"},
+			}
+		}
+	},
+	{--purchase 7 mgun support
+		waveNumber = 7,
+		-- Repeat = 4,
+		minRepeat = 1,
+		maxRepeat = 3,
+        skipPossible = false,		
+		waveDuration = 11,
+		Units = {
+			["ger"] = {--Germany
+			--SPG
+				{priority = 1.0, class = UnitClass.ATTank, unit = "mg34_lafette"},
+			},
+			["rus"] = {--Russia
+			--SPG
+				{priority = 1.0, class = UnitClass.ATTank, unit = "maxim"},
+			}
+		}
+	},
+
+
+}
+
+--Conversion Chart
+--0.9 = 3.5
+--1.0 = 3
+--1.1 = 2.5
+--1.2 = 2
+--1.3 = 1.5
+--1.4 = 1
+--1.5 = 0.5
+--1.6 = 0.1
---
+++ ai_test_system/2524386944/resource/script/multiplayer/bot.data.purchase.conquest.late.5.lua
@@ -0,0 +1,207 @@
+Purchases["campaign_capture_the_flag"] = {
+	{--purchase 1 starting recon wave c 1 по 5 бой
+		divisionName = "Condor Late Tank Division",
+		waveNumber = 1, -- какая по порядку будет
+		-- Repeat = 10, -- сколько раз должно вызваться, чтобы могло перейти к следующей волне
+		minRepeat = 6,
+		maxRepeat = 10,
+		skipPossible = false,  -- ничего не делает
+		waveDuration = 20, -- длительность того, как долго бот будет вызывать данные войска. Каждый юнит вызывается каждые 3 секунды. Если нужно чтобы было только определенное количество то Repeat * 3 < Wave duration  При этом Repeat говорит сколько раз нужно вызвать юнит.
+		Units = {
+			["ger"] = {--Germany
+			--Wheel_vehicles
+				{priority = 1.0, class = UnitClass.Vehicle, unit = "kubelwagen_mg"},
+				{priority = 1.0, class = UnitClass.Vehicle, unit = "sdkfz234_2"},
+				{priority = 0.3, class = UnitClass.Tank, unit = "panzer2l"},
+				{priority = 1.0, class = UnitClass.Vehicle, unit = "sdkfz231"},
+				{priority = 1.0, class = UnitClass.Vehicle, unit = "sdkfz221"},
+			--Infantry Squads
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_recon_con(ger)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_recon_motor_con"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_recon_armor_con"},				
+			},
+			["rus"] = {--Russia
+			--Infantry Singles
+			    {priority = 1.0, class = UnitClass.Infantry, unit = "squad_recon_con(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "single_sniper(rus)"},
+			--Wheel_vehicles
+				{priority = 1.0, class = UnitClass.Vehicle, unit = "ba64"},
+				{priority = 0.2, class = UnitClass.Vehicle, unit = "ba6"},
+				{priority = 1.0, class = UnitClass.Vehicle, unit = "m3a1e1_scout_mgun"},
+				{priority = 0.5, class = UnitClass.Vehicle, unit = "gaz67b"},
+			}
+		}
+	},
+	{--purchase 2 mortar
+		waveNumber = 2,
+	    -- Repeat = 6,
+	    minRepeat = 1,
+		maxRepeat = 2,
+	    skipPossible = false,
+	    waveDuration = 8,
+		Units = {
+			["ger"] = {--Germany
+		    --Howitzers
+                {priority = 1.0, class = UnitClass.ATTank, unit = "80mm_sgrw34"},
+			},
+			["rus"] = {--Russia
+			--Howitzers
+                {priority = 1.0, class = UnitClass.ATTank, unit = "82mm_bm37"},
+			}
+		}
+	},	
+	{--purchase 3 heavy mortar
+		waveNumber = 3,
+	    -- Repeat = 2,
+	    minRepeat = 0,
+		maxRepeat = 0,
+	    skipPossible = false,
+	    waveDuration = 6,
+		Units = {
+			["ger"] = {--Germany
+		    --Howitzers
+                
+			},
+			["rus"] = {--Russia
+			--Howitzers
+                
+			}
+		}
+	},
+	{--purchase 4 heavy tank group
+		waveNumber = 4,
+	    -- Repeat = 17,
+	    minRepeat = 2,
+		maxRepeat = 4,
+	    skipPossible = false,
+	    waveDuration = 20,
+		Units = {
+			["ger"] = {--Germany
+			--Tanks_heavy
+				{priority = 0.8, class = UnitClass.HeavyTank, unit = "panzer5d"},
+				{priority = 0.4, class = UnitClass.HeavyTank, unit = "panzer5a"},
+				{priority = 0.5, class = UnitClass.HeavyTank, unit = "panzer5g"},				
+			},
+			["rus"] = {--Russia				
+			--Tanks_heavy
+			}
+		}
+	},
+	{--purchase 5 medium tank group
+		waveNumber = 5,
+	    -- Repeat = 20,
+	    minRepeat = 2,
+		maxRepeat = 4,
+	    skipPossible = false,
+	    waveDuration = 20,
+		Units = {
+			["ger"] = {--Germany
+			--Tanks_medium
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer4h"},
+				{priority = 0.5, class = UnitClass.Tank, unit = "panzer4j"},
+				{priority = 0.2, class = UnitClass.Tank, unit = "panzer4g"},
+			},
+			["rus"] = {--Russia		
+			--Tanks_medium					
+                {priority = 1.2, class = UnitClass.Tank, unit = "t3485_44"},
+				{priority = 0.7, class = UnitClass.Tank, unit = "t3476_43"},
+				{priority = 0.1, class = UnitClass.Tank, unit = "t3457_43"},
+			}
+		}
+	},
+	{--purchase 6 light tank group
+		waveNumber = 6,
+	    -- Repeat = 10,
+	    minRepeat = 2,
+		maxRepeat = 4,
+	    skipPossible = false,
+	    waveDuration = 20,
+		Units = {
+			["ger"] = {--Germany
+			--Tanks_light
+                
+				
+			},
+			["rus"] = {--Russia			
+            --Tanks_light
+				{priority = 0.2, class = UnitClass.Tank, unit = "t60"},
+				{priority = 0.4, class = UnitClass.Tank, unit = "t70"},
+				{priority = 0.4, class = UnitClass.Tank, unit = "t70m"},			
+			}
+		}
+	},
+	{--purchase 7 infantry support
+		waveNumber = 7,
+		-- Repeat = 50,
+		minRepeat = 4,
+		maxRepeat = 6,
+        skipPossible = false,		
+		waveDuration = 35,
+		Units = {
+			["ger"] = {--Germany
+			--Infantry Squads
+			    {priority = 1.0, class = UnitClass.Infantry, unit = "squad_regular_motor_con"},
+				{priority = 0.2, class = UnitClass.Infantry, unit = "squad_pz_pio_con"},
+				{priority = 0.1, class = UnitClass.Infantry, unit = "single_officer(ger)"},
+			},
+			["rus"] = {--Russia
+			--Infantry Squads
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_smg_con(rus)"},
+				{priority = 0.1, class = UnitClass.Infantry, unit = "single_officer(rus)"},
+			}
+		}
+	},
+	{--purchase 8 SPG support
+		waveNumber = 8,
+		-- Repeat = 12,
+		minRepeat = 2,
+		maxRepeat = 4,
+        skipPossible = false,		
+		waveDuration = 15,
+		Units = {
+			["ger"] = {--Germany
+			--SPG
+				{priority = 1.0, class = UnitClass.ATTank, unit = "marder3m"},
+				{priority = 0.1, class = UnitClass.ArtilleryTank, unit = "sig33b"},
+			},
+			["rus"] = {--Russia
+			--SPG
+				{priority = 1.0, class = UnitClass.ATTank, unit = "su85"},
+				{priority = 0.2, class = UnitClass.ArtilleryTank, unit = "su122"},
+			}
+		}
+	},
+	{--purchase 11 AA support
+		waveNumber = 11,
+		-- Repeat = 12,
+		minRepeat = 1,
+		maxRepeat = 2,
+        skipPossible = false,		
+		waveDuration = 15,
+		Units = {
+			["ger"] = {--Germany
+			--AA
+			    {priority = 0.6, class = UnitClass.Vehicle, unit = "blitz3_6_flak30"},	
+				{priority = 0.4, class = UnitClass.ATTank, unit = "37mm_flak37"},
+				{priority = 0.2, class = UnitClass.ATTank, unit = "88mm_flak36"},			
+				{priority = 0.1, class = UnitClass.Tank, unit = "grille_m"},
+			},
+			["rus"] = {--Russia
+			--AA
+				{priority = 0.5, class = UnitClass.ATTank, unit = "25mm_72k"},
+				{priority = 0.5, class = UnitClass.ATTank, unit = "37mm_61k"},
+				{priority = 0.2, class = UnitClass.Vehicle, unit = "gaz_aaa_72k"},
+			}
+		}
+	},
+}
+
+--Conversion Chart
+--0.9 = 3.5
+--1.0 = 3
+--1.1 = 2.5
+--1.2 = 2
+--1.3 = 1.5
+--1.4 = 1
+--1.5 = 0.5
+--1.6 = 0.1
