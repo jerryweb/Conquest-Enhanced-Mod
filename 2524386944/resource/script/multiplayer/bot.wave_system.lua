1e5d23a083c89fadba7d75c908f1894b76eb7974
Jerry Webb <ljwebbdev8800@gmail.com>
2022-01-14 01:21:01 -0800

    removed testing var

--- 2524386944/resource/script/multiplayer/bot.wave_system.lua
+++ 2524386944/resource/script/multiplayer/bot.wave_system.lua
@@ -1,7 +1,7 @@
 require([[/script/multiplayer/bot.data]])
 
 -- This enables early testing so that units come 30 seconds after start. 
-testing = true
+testing = false
 
 -- Number of unit division roster files to randomly select for each period in the war 
 maxNumOfEarlyDivisions = 5
