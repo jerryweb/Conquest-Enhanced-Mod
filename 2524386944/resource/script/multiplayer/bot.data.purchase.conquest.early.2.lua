11719900d3c1daf677dae736ab882a5d3c11ba4c
Jerry Webb <ljwebbdev8800@gmail.com>
2022-01-21 00:10:57 -0800

    Trying to fix weird incorrect push

---
+++ 2524386944/resource/script/multiplayer/bot.data.purchase.conquest.early.2.lua
@@ -0,0 +1,306 @@
+Purchases["campaign_capture_the_flag"] = {
+	{--purchase 1 INFANTRY AND SUPPORT WEAPON BASED 
+		divisionName = "INFANTRY AND SUPPORT WEAPON",
+		waveNumber = 1, -- какая по порядку будет
+		-- Repeat = 6, -- сколько раз должно вызваться, чтобы могло перейти к следующей волне
+		minRepeat = 2,
+		maxRepeat = 5,
+		skipPossible = false,  -- ничего не делает
+		waveDuration = 20, -- длительность того, как долго бот будет вызывать данные войска. Каждый юнит вызывается каждые 3 секунды. Если нужно чтобы было только определенное количество то Repeat * 3 < Wave duration  При этом Repeat говорит сколько раз нужно вызвать юнит.
+		Units = {
+			["ger"] = {--Germany
+			--Wheel_vehicles
+				{priority = 1.0, class = UnitClass.Vehicle, unit = "kubelwagen_mg"},
+				{priority = 1.5, class = UnitClass.Vehicle, unit = "sdkfz221"},
+				{priority = 1.5, class = UnitClass.Vehicle, unit = "sdkfz231"},
+			--Infantry Squads
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_recon_con(ger)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_recon_armor_con"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_sicherung_con(ger)"},
+				{priority = 1.5, class = UnitClass.Infantry, unit = "squad_regular_con(ger)"},
+			},
+			["rus"] = {--Russia
+			--Infantry Singles
+			    {priority = 2.5, class = UnitClass.Infantry, unit = "squad_recon_con(rus)"},
+		        {priority = 1.0, class = UnitClass.Infantry, unit = "squad_border(rus)"},
+			--Wheel_vehicles
+				{priority = 1.5, class = UnitClass.Vehicle, unit = "gaz67b"},
+				{priority = 1.5, class = UnitClass.Vehicle, unit = "ba20"},
+			}
+		}
+	},
+	{--purchase 3 tank assault group
+		waveNumber = 2,
+	    -- Repeat = 10,
+	    minRepeat = 2,
+		maxRepeat = 4,
+	    skipPossible = false,
+	    waveDuration = 20,
+		Units = {
+			["ger"] = {--Germany
+			--Tanks_medium
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer3e"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer3f"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer3j"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer4d"},
+				{priority = 0.1, class = UnitClass.Tank, unit = "panzer4f1"},
+			--Tanks_light
+				{priority = 0.5, class = UnitClass.Tank, unit = "panzer1b"},
+				{priority = 2.5, class = UnitClass.Tank, unit = "panzer2c"},
+				{priority = 0.6, class = UnitClass.Tank, unit = "panzer2f"},
+				{priority = 0.7, class = UnitClass.Tank, unit = "panzer35t"},
+				{priority = 2.2, class = UnitClass.Tank, unit = "pz38t_a"},
+				{priority = 1.2, class = UnitClass.Tank, unit = "pz38t_f"},
+			},
+			["rus"] = {--Russia
+			--Tanks_heavy
+				{priority = 0.1, class = UnitClass.HeavyTank, unit = "kv1_40"},
+		    --Tanks_medium
+				{priority = 0.5, class = UnitClass.Tank, unit = "t28"},
+				{priority = 1.5, class = UnitClass.Tank, unit = "bt5"},
+			--Tanks_light
+			   	{priority = 2.0, class = UnitClass.Tank, unit = "t26_33"},
+				{priority = 2.0, class = UnitClass.Tank, unit = "t26_39"},
+					{priority = 0.5, class = UnitClass.Tank, unit = "t3476_40"},
+				{priority = 0.5, class = UnitClass.Tank, unit = "t3476_41"},
+		    }
+		}
+	},
+	{--purchase 2 mortar
+		waveNumber = 3,
+	    -- Repeat = 2,
+	    minRepeat = 1,
+		maxRepeat = 2,
+	    skipPossible = false,
+	    waveDuration = 5,
+	    isHeavyArty = true,
+		Units = {
+			["ger"] = {--Germany
+				--Howitzers
+				{priority = 2.0, class = UnitClass.ATTank, unit = "80mm_sgrw34"},
+			},
+			["rus"] = {--Russia
+			--Howitzers
+				{priority = 2.0, class = UnitClass.ATTank, unit = "82mm_bm37"},
+			}
+		}
+	},
+	{--purchase 4 field gun support
+		waveNumber = 4,
+		-- Repeat = 3,
+		minRepeat = 2,
+		maxRepeat = 4,
+        skipPossible = false,		
+		waveDuration = 8,
+		Units = {
+			["ger"] = {--Germany
+			--SPG
+				{priority = 1.5, class = UnitClass.ATTank, unit = "37mm_pak36"},
+				{priority = 0.5, class = UnitClass.ATTank, unit = "47mm_pakt"},
+				{priority = 1.5, class = UnitClass.ArtilleryTank, unit = "75mm_leig18"},
+			},
+			["rus"] = {--Russia
+			--SPG
+				{priority = 1.5, class = UnitClass.ATTank, unit = "45mm_m37"},
+				{priority = 1.0, class = UnitClass.ATTank, unit = "45mm_m42"},
+				{priority = 1.5, class = UnitClass.ArtilleryTank, unit = "76mm_m1927"},
+			}
+		}
+	},
+	{--purchase 5 infantry support
+		waveNumber = 5,
+		-- Repeat = 10,
+		minRepeat = 5,
+		maxRepeat = 8,
+        skipPossible = false,		
+		waveDuration = 35,
+		Units = {
+			["ger"] = {--Germany
+			--Infantry Squads
+				{priority = 1.5, class = UnitClass.Infantry, unit = "squad_osttruppen_con(ger)"},
+				{priority = 0.75, class = UnitClass.Infantry, unit = "squad_strafbatallion_con(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_luftwaffe_con(ger)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_sicherung_con(ger)"},
+				{priority = 3.5, class = UnitClass.Infantry, unit = "squad_regular_con(ger)"},
+					{priority = 2.0, class = UnitClass.ATInfantry, unit = "single_at(ger)"},
+				{priority = 2.0, class = UnitClass.ATInfantry, unit = "single_riflegrenade(ger)"},	
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_regular_vet_con(ger)"},
+				{priority = 1.5, class = UnitClass.Infantry, unit = "squad_blau_con(ger)"},
+				{priority = 1.5, class = UnitClass.Infantry, unit = "single_flamer(ger)"},
+				{priority = 1.5, class = UnitClass.Infantry, unit = "squad_pionier_con(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_pz_pio_con"},
+			},
+			["rus"] = {--Russia
+			--Infantry Squads
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_partisan_con(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_penal_con(rus)"},
+
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_border(rus)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_conscripts_con(rus)"},
+				{priority = 2.5, class = UnitClass.Infantry, unit = "squad_rifleunit_alt_con(rus)"},
+				{priority = 2.0, class = UnitClass.ATInfantry, unit = "single_at(rus)"},
+				{priority = 1.5, class = UnitClass.ATInfantry, unit = "single_riflegrenade(rus)"},
+				{priority = 3.5, class = UnitClass.Infantry, unit = "squad_rifleunit_con(rus)"},
+				{priority = 1.5, class = UnitClass.Infantry, unit = "squad_mountainrifles_con(rus)"},
+				{priority = 1.5, class = UnitClass.Infantry, unit = "squad_guardsrifle_con(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_guards_smg_con(rus)"},
+		    }
+		}
+	},
+
+	{--purchase 6 SPG support
+		waveNumber = 6,
+		-- Repeat = 3,
+		minRepeat = 1,
+		maxRepeat = 3,
+        skipPossible = false,		
+		waveDuration = 5,
+		Units = {
+			["ger"] = {--Germany
+			--SPG
+				{priority = 0.5, class = UnitClass.ATTank, unit = "panzerjager1"},
+				{priority = 1.5, class = UnitClass.ATTank, unit = "stug3b"},
+			},
+			["rus"] = {--Russia
+			--SPG
+				{priority = 0.1, class = UnitClass.ATTank, unit = "zis30"},
+			}
+		}
+	},
+	{--purchase 7 Tank 2
+		waveNumber = 7,
+		-- Repeat = 5,
+		minRepeat = 1,
+		maxRepeat = 3,
+        skipPossible = false,		
+		waveDuration = 20,
+		Units = {
+			["ger"] = {--Germany
+			--Tanks_medium
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer3e"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer3f"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer3j"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer4d"},
+				{priority = 0.1, class = UnitClass.Tank, unit = "panzer4f1"},
+			--Tanks_light
+				{priority = 0.5, class = UnitClass.Tank, unit = "panzer1b"},
+				{priority = 2.5, class = UnitClass.Tank, unit = "panzer2c"},
+				{priority = 0.6, class = UnitClass.Tank, unit = "panzer2f"},
+				{priority = 0.7, class = UnitClass.Tank, unit = "panzer35t"},
+				{priority = 2.2, class = UnitClass.Tank, unit = "pz38t_a"},
+				{priority = 1.2, class = UnitClass.Tank, unit = "pz38t_f"},
+			},
+			["rus"] = {--Russia
+			--Tanks_heavy
+				{priority = 0.1, class = UnitClass.HeavyTank, unit = "kv1_40"},
+		    --Tanks_medium
+				{priority = 0.5, class = UnitClass.Tank, unit = "t28"},
+				{priority = 1.5, class = UnitClass.Tank, unit = "bt5"},
+			--Tanks_light
+			   	{priority = 2.0, class = UnitClass.Tank, unit = "t26_33"},
+				{priority = 2.0, class = UnitClass.Tank, unit = "t26_39"},
+					{priority = 0.5, class = UnitClass.Tank, unit = "t3476_40"},
+				{priority = 0.5, class = UnitClass.Tank, unit = "t3476_41"},
+		    }
+		}
+	},
+	{--purchase 8 infantry attack 2
+		waveNumber = 8,
+		-- Repeat = 10,
+		minRepeat = 4,
+		maxRepeat = 11,
+        skipPossible = false,		
+		waveDuration = 35,
+		Units = {
+			["ger"] = {--Germany
+			--Infantry Squads
+				{priority = 1.5, class = UnitClass.Infantry, unit = "squad_osttruppen_con(ger)"},
+				{priority = 0.75, class = UnitClass.Infantry, unit = "squad_strafbatallion_con(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_luftwaffe_con(ger)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_sicherung_con(ger)"},
+				{priority = 3.5, class = UnitClass.Infantry, unit = "squad_regular_con(ger)"},
+					{priority = 2.0, class = UnitClass.ATInfantry, unit = "single_at(ger)"},
+				{priority = 2.0, class = UnitClass.ATInfantry, unit = "single_riflegrenade(ger)"},	
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_regular_vet_con(ger)"},
+				{priority = 1.5, class = UnitClass.Infantry, unit = "squad_blau_con(ger)"},
+				{priority = 1.5, class = UnitClass.Infantry, unit = "single_flamer(ger)"},
+				{priority = 1.5, class = UnitClass.Infantry, unit = "squad_pionier_con(ger)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_pz_pio_con"},
+			},
+			["rus"] = {--Russia
+			--Infantry Squads
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_partisan_con(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_penal_con(rus)"},
+
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_border(rus)"},
+				{priority = 2.0, class = UnitClass.Infantry, unit = "squad_conscripts_con(rus)"},
+				{priority = 2.5, class = UnitClass.Infantry, unit = "squad_rifleunit_alt_con(rus)"},
+				{priority = 2.0, class = UnitClass.ATInfantry, unit = "single_at(rus)"},
+				{priority = 1.5, class = UnitClass.ATInfantry, unit = "single_riflegrenade(rus)"},
+				{priority = 3.5, class = UnitClass.Infantry, unit = "squad_rifleunit_con(rus)"},
+				{priority = 1.5, class = UnitClass.Infantry, unit = "squad_mountainrifles_con(rus)"},
+				{priority = 1.5, class = UnitClass.Infantry, unit = "squad_guardsrifle_con(rus)"},
+				{priority = 1.0, class = UnitClass.Infantry, unit = "squad_guards_smg_con(rus)"},
+		    }
+		}
+	},
+	{--purchase 9 mgun support
+		waveNumber = 9,
+		Repeat = 3,
+		minRepeat = 1,
+		maxRepeat = 2,
+        skipPossible = false,		
+		waveDuration = 7,
+		Units = {
+			["ger"] = {--Germany
+			--SPG
+				{priority = 1.0, class = UnitClass.ATTank, unit = "mg34_lafette"},
+			},
+			["rus"] = {--Russia
+			--SPG
+				{priority = 1.0, class = UnitClass.ATTank, unit = "maxim"},
+				{priority = 1.0, class = UnitClass.Vehicle, unit = "sg43_stand"},
+			}
+		}
+	},
+	{--purchase 10 light tank rush
+		waveNumber = 10,
+		-- Repeat = 10,
+		minRepeat = 3,
+		maxRepeat = 6,
+        skipPossible = false,		
+		waveDuration = 10,
+		Units = {
+			["ger"] = {--Germany
+			--Wheel_vehicles
+			{priority = 0.7, class = UnitClass.Tank, unit = "panzer1b"},
+				{priority = 2.0, class = UnitClass.Tank, unit = "panzer2c"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "panzer2f"},
+				{priority = 0.5, class = UnitClass.Tank, unit = "panzer35t"},
+				{priority = 2.5, class = UnitClass.Tank, unit = "pz38t_a"},
+				{priority = 0.5, class = UnitClass.Tank, unit = "pz38t_f"},
+			},
+			["rus"] = {--Russia
+            --Tanks_light			
+			   {priority = 0.5, class = UnitClass.Tank, unit = "t26_31"},
+				{priority = 1.0, class = UnitClass.Tank, unit = "t26_33"},
+				{priority = 2.0, class = UnitClass.Tank, unit = "t26_39"},
+				{priority = 2.0, class = UnitClass.Tank, unit = "t37a"},
+				{priority = 0.1, class = UnitClass.Tank, unit = "bt2"},
+				{priority = 2.5, class = UnitClass.Tank, unit = "bt5"},
+					{priority = 0.5, class = UnitClass.Tank, unit = "t40"},
+				{priority = 3.0, class = UnitClass.Tank, unit = "t60"},
+			}
+		}
+	}
+}
+
+--Conversion Chart
+--0.9 = 3.5
+--1.0 = 3
+--1.1 = 2.5
+--1.2 = 2
+--1.3 = 1.5
+--1.4 = 1
+--1.5 = 0.5
+--1.6 = 0.1
---
+++ 2524386944/resource/script/multiplayer/bot.lua
@@ -0,0 +1,527 @@
+require([[/script/multiplayer/bot.data]])
+require([[/script/multiplayer/bot.wave_system]])
+
+local squadDictionary = {}
+
+local emplacementArtillery = {}
+local mortars = {}
+local mortarCounter = 0
+local IsHeavyArty = false
+local heavyArtyCounter = 0
+local artyOrderRotationPeriod = 60 * 500
+
+
+local Context = {
+	Purchase = nil,
+	SpawnInfo = nil,
+	SpawnWait = {
+		CooldownTimer = nil,
+		WaitTimer = nil
+	},
+	SquadTimers = {},
+	IsHeavyArty = false,
+	IsMortar = false
+}
+
+function SetSpawnCooldownTimer()
+	Context.SpawnWait.CooldownTimer = BotApi.Events:SetQuantTimer(
+		function()
+			Context.SpawnWait.CooldownTimer = nil
+		end,
+		math.random(SpawnCooldownTime.Min, SpawnCooldownTime.Max))
+end
+
+function KillSpawnCooldownTimer()
+	if Context.SpawnWait.CooldownTimer then
+		BotApi.Events:KillQuantTimer(Context.SpawnWait.CooldownTimer)
+		Context.SpawnWait.CooldownTimer = nil
+	end
+end
+
+function KillSpawnWaitTimer()
+	if Context.SpawnWait.WaitTimer then
+		BotApi.Events:KillQuantTimer(Context.SpawnWait.WaitTimer)
+		Context.SpawnWait.WaitTimer = nil
+	end
+end
+
+function GetRandomItem(items, getRate)
+	local item_rates = {}
+	
+	local total = 0
+	for i, item in pairs(items) do
+		local rate = getRate(item)
+		total = total + rate
+		table.insert(item_rates, {i = item, r = rate})
+	end
+	
+	local rnd = math.random()
+	local bound = 0.0
+	
+	for j, item_rate in pairs(item_rates) do
+		bound = bound + item_rate.r	
+		if rnd < bound / total then
+			return item_rate.i
+		end
+	end
+end
+
+function IsCapturedFlag(flag)
+	return flag.occupant == BotApi.Instance.team
+end
+
+function IsEnemyFlag(flag)
+	return flag.occupant == BotApi.Instance.enemyTeam
+end
+
+function GetFlagPriority(flag)
+	if		IsCapturedFlag(flag)then return FlagPriority.Enemy
+	elseif	IsEnemyFlag(flag)	then return FlagPriority.Enemy
+	else 							 return FlagPriority.Neutral
+	end
+end
+
+function GetFlagToCapture(flagPoints, getPriority)
+	local flags = {}
+	
+	for i, flag in pairs(flagPoints) do
+		-- print("flag name: ", flag.name)
+		table.insert(flags, {name = flag.name, k = getPriority(flag)})
+	end
+
+	return GetRandomItem(flags, function(f) return f.k end)
+end
+
+function GetNextUnitToSpawn(purchase)
+	-- local units = purchase:current()
+	-- print("Grabbing unlocked units for wave: ", purchase.idx)
+	local units = purchase.unlockedUnits
+	IsHeavyArty = purchase.isHeavyArty
+	IsMortar = purchase.isMortar
+
+	if not units then
+		print("wave ", purchase.idx, " has no available units")
+		return nil
+	end
+	
+	-- local unit = GetUnitToSpawn(units[BotApi.Instance.army])
+	local unit = GetUnitToSpawn(units)
+	purchase:moveNext()
+	return unit
+end
+
+function GetUnitToSpawn(units)
+	if not units then
+		return nil
+	end
+	
+	local unitsToSpawn = {}
+	
+	local teamSize = BotApi.Instance.teamSize
+	local income = BotApi.Commands:Income(BotApi.Instance.playerId)
+
+	for i, unit in pairs(units) do
+
+		local min_income = unit.min_income
+		local min_team = unit.min_team
+		
+		if not min_income then min_income = -1 end
+		if not min_team then min_team = 0 end
+		
+		if teamSize >= min_team and income >= min_income then
+			table.insert(unitsToSpawn, unit)
+		end
+		
+	end
+	
+	if #unitsToSpawn == 0 then
+		return nil
+	end
+	
+	local capturedFlags, enemyFlags = 0, 0
+	for i, flag in pairs(BotApi.Scene.Flags) do
+		if IsCapturedFlag(flag) then
+			capturedFlags = capturedFlags + 1
+		end
+		if IsEnemyFlag(flag) then
+			enemyFlags = enemyFlags + 1
+		end
+	end
+	
+	if capturedFlags < enemyFlags then
+		choice = 0.10
+	elseif capturedFlags == enemyFlags then
+		choice = 0.20
+	else
+		choice = 0.30	-- higher choice(0-1+rnd) = higher % of CaptureFlag(bots going for flags). Lower choice favors SeekAndDestroy.
+	end
+	
+	local enemyHasTanks = BotApi.Commands:EnemyHasTanks();
+	
+	return GetRandomItem(unitsToSpawn, function(t)
+		if capturedFlags <= enemyFlags and (t.class == UnitClass.Tank or
+							                t.class == UnitClass.Infantry) then
+			return t.priority * 2.0
+		end
+		
+		if enemyHasInfantry and (t.class == UnitClass.Tank or
+							    t.class == UnitClass.ArtilleryTank) then
+			return t.priority * 1.5
+		end
+		
+		if enemyHasTanks and (t.class == UnitClass.ATTank or
+		                      t.class == UnitClass.Tank or
+							  t.class == UnitClass.HeavyTank) then
+			return t.priority * 2.0
+		end
+		return t.priority
+	end)
+end
+
+function UpdateUnitToSpawn(purchase)
+	Context.SpawnInfo = GetNextUnitToSpawn(purchase)
+end
+
+function OnGameStart()
+	math.randomseed(os.time()*BotApi.Instance.hostId)
+	math.random() math.random() math.random() math.random()
+	print("TESTING MODE ACTIVATED = ", testing)
+	gameStartTime = os.clock()
+	
+	local totalFlags = 0
+	for i, flag in pairs(BotApi.Scene.Flags) do
+		print("flag name: ", flag.name)
+		totalFlags = totalFlags + 1
+	end
+
+	print("Total flags detected:", totalFlags)
+
+	if not testing then
+		if totalFlags >= 4 then
+			if math.random() < 0.5 then -- 50% chance to change when enemy reinforcements spawn 
+				firstWaveOffsetTime = math.random(480,780) 
+				print("changing first wave offset to ", firstWaveOffsetTime)
+			end
+		else 
+			if math.random() < 0.3 then -- 30% chance to change when enemy reinforcements spawn 
+				firstWaveOffsetTime = math.random(480,780) 
+				print("changing first wave offset to ", firstWaveOffsetTime)
+			end
+		end
+	end
+	if not testing then
+		if math.random() < 0.5 then -- 50% chance to for typhoon wave mode 
+			setTyphoonWaveMode()
+		end
+	end
+	-- 30% chance to toggle ingame typhoon wave mode 
+	if math.random() < 0.3 then
+		activateToggleTyphoonWaveMode()
+	end
+
+	
+	print("first enemy wave will start at ", gameStartTime + firstWaveOffsetTime)
+	nextTyphoonWaveTime = gameStartTime + firstWaveOffsetTime
+	nextTyphoonWaveToggleTime = gameStartTime + typhoonWaveToggleInterval + firstWaveOffsetTime
+	print("Next typhoon wave toggle time will start at ", gameStartTime + firstWaveOffsetTime)
+
+	local purchasesModule = [[/script/multiplayer/bot.data.purchase.]] .. BotApi.Instance.gameMode;
+	if module_exists(purchasesModule) then
+		require(purchasesModule)
+	end
+
+	local purchases = Purchases[BotApi.Instance.gameMode];
+	if not purchases then
+		purchases = {}
+	end
+		
+
+	if BotApi.Instance.gameMode == "campaign_capture_the_flag" then
+		print("getting ready to load roster")
+	    local divisionPurchases = {}
+	    local counter = 0
+	    local spawnMultiplier = 5 - totalFlags
+		local purchaseModel = selectArmyDivision(totalFlags)
+
+		if module_exists(purchaseModel) then
+	      require(purchaseModel)
+		  print("model loading")
+	      divisionPurchases = Purchases["campaign_capture_the_flag"]
+		  print("model loaded")
+	    else
+		print("model failed to load")
+	      local limit = totalFlags + 1
+	      for key, value in pairs(purchases) do
+	        if counter < limit then
+	          table.insert(divisionPurchases, value)
+	          counter = counter + 1
+	        end
+	      end
+	    end
+	    
+	    SpawnCooldownTime.Min = SpawnCooldownTime.Min * spawnMultiplier
+	    SpawnCooldownTime.Max = SpawnCooldownTime.Max * spawnMultiplier
+	    defaultSpawnCooldownTime = SpawnCooldownTime
+
+	    if typhoonWaveMode then
+	    	 setTyphoonWaveMode()
+	    end
+	    
+	    print("Spawn multipliers (min, max):", SpawnCooldownTime.Min, SpawnCooldownTime.Max)
+	    purchases = divisionPurchases
+	end
+
+	Context.Purchase = PIter:new(purchases)
+	UpdateUnitToSpawn(Context.Purchase)
+	SetSpawnCooldownTimer()
+
+	-- print("BotApi.Commands properties:")
+	-- print(BotApi.Commands)
+	-- local count = 0
+	-- for i, BotApi.Scene in pairs(metatable(BotApi.Scene)) do 
+	-- 	print("stuffs ", i)
+	-- 	-- count = count + 1
+	-- 	-- print(i, "properties")
+	-- 	-- for j, BotApi[1] in pairs(BotApi[count]) do
+	-- 	-- 	print(j)
+	-- 	-- end
+	-- end
+end
+
+function OnGameStop()
+	KillSpawnCooldownTimer()
+	KillSpawnWaitTimer()
+
+	for squad, timer in pairs(Context.SquadTimers) do
+		if timer then
+			BotApi.Events:KillQuantTimer(timer)
+		end
+	end
+	collectgarbage("collect")
+end
+
+function TrySpawnUnit()
+	if Context.SpawnWait.CooldownTimer then
+		return
+	end
+
+	if not BotApi.Commands:CanSpawn() then
+		return
+	end
+	
+	if not Context.SpawnInfo then
+		UpdateUnitToSpawn(Context.Purchase)
+		return
+	end
+
+	local unit = Context.SpawnInfo.unit
+	
+	if not BotApi.Commands:IsUnitAvailable(unit) then
+		-- print(unit, "not available, player#" .. BotApi.Instance.playerId)
+		KillSpawnWaitTimer()
+		UpdateUnitToSpawn(Context.Purchase)
+		return
+	end
+	if currentWaveMaxUnitCount > 0 then
+		if BotApi.Commands:Spawn(unit, MaxSquadSize) then
+			if IsHeavyArty then 
+				heavyArtyCounter = heavyArtyCounter + 1
+				print("incrementing arty counter")
+			end
+
+			-- if IsMortar then 
+			-- 	mortarCounter = mortarCounter + 1
+			-- 	print("incrementing mortar counter")
+			-- end
+			currentWaveMaxUnitCount = currentWaveMaxUnitCount - 1
+			KillSpawnWaitTimer()
+			SetSpawnCooldownTimer()
+			UpdateUnitToSpawn(Context.Purchase)
+			return
+		end
+	else
+		-- print("can't spawn ", unit, ", max number of squads for this wave reached")
+		UpdateUnitToSpawn(Context.Purchase)
+		return
+	end 
+
+	local tts = BotApi.Commands:TimeToSpawnUnit(unit)
+	if tts > UnitSpawnWaitTime then
+		print(unit, tts, "wait too long, player#" .. BotApi.Instance.playerId)
+		KillSpawnWaitTimer()
+		UpdateUnitToSpawn(Context.Purchase)
+		return
+	end
+
+	if not Context.SpawnWait.WaitTimer then
+		print(unit, tts, "set wait timer, player#" .. BotApi.Instance.playerId)
+		Context.SpawnWait.WaitTimer = BotApi.Events:SetQuantTimer(
+			function()
+				Context.SpawnWait.WaitTimer = nil
+				UpdateUnitToSpawn(Context.Purchase)
+			end, UnitSpawnWaitTime + 1000)
+	end
+end
+
+function OnGameQuant()
+
+	if typhoonWaveModeInGameToggle then
+		if os.clock() > nextTyphoonWaveToggleTime then
+			if math.random() < 0.5 then 
+				print("Flipping typhoon wave mode")
+				if typhoonWaveMode then
+					disableTyphoonWaveMode()
+				else
+					setTyphoonWaveMode()
+				end
+			end
+			nextTyphoonWaveToggleTime = os.clock() + typhoonWaveToggleInterval
+		end 
+	end
+
+	if os.clock() > (gameStartTime + firstWaveOffsetTime) then
+		if typhoonWaveMode then
+			if os.clock() > nextTyphoonWaveTime then
+				TrySpawnUnit()
+				if os.clock() > nextTyphoonWaveTime + typhoonWaveDuration then
+					setNextTyphoonWaveTime()
+				end
+			end
+		else 
+			TrySpawnUnit()
+		end
+	end
+
+
+	local waypoints = BotApi.Scene.Waypoints
+
+	if #waypoints == 0 then
+		for i, squad in pairs(BotApi.Scene.Squads) do
+			-- print("squad ", squad, " with unlock time = ", squadDictionary[squad])
+			if emplacementArtillery[squad] then 
+				if not Context.SquadTimers[squad] then
+					SetSquadOrder(CaptureFlag, squad, artyOrderRotationPeriod)
+				end
+			end
+			-- if mortars[squad] then 
+			-- 	if not Context.SquadTimers[squad] then
+			-- 		SetSquadOrder(CaptureFlag, squad, OrderRotationPeriod)
+			-- 	end
+			-- end
+			if squadDictionary[squad] and  squadDictionary[squad] <= os.clock() then
+				if not Context.SquadTimers[squad] then
+					SetSquadOrder(CaptureFlag, squad, OrderRotationPeriod)
+				end
+			end
+			 if not squadDictionary[squad] then
+			 	local squadOrderTime = math.random(180, 300)
+			 	print("nil squad ", squad, " with unlock time = ", squadDictionary[squad])
+				squadDictionary[squad] = os.clock() + squadOrderTime
+			end
+		end
+	end
+end
+
+function SeekAndDestroy(squad)
+	BotApi.Commands:SeekAndDestroy(squad)
+end
+
+function GotoNextWaypoint(squad)
+	local waypoints = BotApi.Scene.Waypoints
+	BotApi.Commands:CaptureFlag(squad, waypoints[math.random(#waypoints)]) --captureflag is basically gothereandattack
+	print("#captureFlag call inside GoToNextWaypoint")
+end
+
+function OnWaypoint(args)
+	print("#GotoNextWaypoint call inside OnWaypoint")
+	GotoNextWaypoint(args.squadId)
+end
+
+function CaptureFlag(squad)
+	local flag = GetFlagToCapture(BotApi.Scene.Flags, GetFlagPriority)
+	-- local rnd = 0.1 + choice
+	local rnd = math.random() + choice
+
+	-- Mortars seem to crash the game when sending them to capture a flag. Will need to investigate
+	if flag then
+		-- if emplacementArtillery[squad] then
+			print("+SeekAndDestroy with  squad", squad)
+			BotApi.Commands:SeekAndDestroy(squad)
+		-- if rnd < 0.25 then
+		-- 	print(rnd, "+SeekAndDestroy with squad", squad)
+		-- 	BotApi.Commands:SeekAndDestroy(squad)
+		-- else
+		-- 	print(rnd, "+CaptureFlag with squad", squad)
+		-- 	if not mortars[squad] then
+		-- 		BotApi.Commands:CaptureFlag(squad, flag.name)
+		-- 	else 
+		-- 		print("oops mortar")
+		-- 		BotApi.Commands:SeekAndDestroy(squad)
+		-- 	end
+		-- end
+	else
+			print(rnd, "!SeekAndDestroy with squad", squad)
+			BotApi.Commands:SeekAndDestroy(squad)
+	end
+end
+
+function SetSquadOrder(order, squad, delay)
+	order(squad)
+	local setTimer = function(callback)
+		Context.SquadTimers[squad] = BotApi.Events:SetQuantTimer(
+			function()
+				order(squad)
+				Context.SquadTimers[squad] = nil
+				if BotApi.Scene:IsSquadExists(squad) then
+					callback(callback)
+				end
+			end,
+			delay)
+	end
+	print("squad timer: ", Context.SquadTimers[squad] )
+	setTimer(setTimer)--
+end
+
+function OnGameSpawn(args)
+	if heavyArtyCounter > 0 then
+		emplacementArtillery[args.squadId] = args
+		print("added ", args.squadId, " to heavy arty list")
+		IsHeavyArty = false
+		heavyArtyCounter = heavyArtyCounter - 1
+		local waypoints = BotApi.Scene.Waypoints
+		if #waypoints == 0 then
+			SetSquadOrder(CaptureFlag, args.squadId, artyOrderRotationPeriod)
+		else
+			GotoNextWaypoint(args.squadId)
+			print("#waypoints != 0")
+		end
+	else 
+
+		local squadOrderTime = math.random(240, 300)
+		squadDictionary[args.squadId] = os.clock() + squadOrderTime
+	end
+
+	-- local waypoints = BotApi.Scene.Waypoints
+
+	-- for i, waypoints in pairs(waypoints) do
+	-- 	print("points", i)
+	-- end
+	-- print("spawned squad id: ", tostring(args.unit))
+
+	
+	-- print("squad time = ", squadDictionary[args.squadId])
+	-- table.insert(squadDictionary, {squadid = args.squadId, unlockTime = os.clock + 10})
+
+	-- if #waypoints == 0 then
+	-- 	SetSquadOrder(CaptureFlag, args.squadId, OrderRotationPeriod)
+	-- else
+	-- 	GotoNextWaypoint(args.squadId)
+	-- 	print("#waypoints != 0")
+	-- end
+end
+
+BotApi.Events:Subscribe(BotApi.Events.GameStart, OnGameStart)
+BotApi.Events:Subscribe(BotApi.Events.GameEnd, OnGameStop)
+BotApi.Events:Subscribe(BotApi.Events.Quant, OnGameQuant)
+BotApi.Events:Subscribe(BotApi.Events.GameSpawn, OnGameSpawn)
+BotApi.Events:Subscribe(BotApi.Events.Waypoint, OnWaypoint)
